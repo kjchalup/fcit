bc033d4f956d404df9a3acec13f702476c3aa4c8
kjchalup <janchatko@gmail.com>
2017-07-09 16:00:05 -0700

    Moved to a flat structure.
    There is only one file, dtit.py, that keeps all the code.

--- dtit/__init__.py
+++
@@ -1,1 +0,0 @@
-from dtit.dtit import test
--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from distutils.core import setup
 
 def read(fnam):
     """ Utility function for README file."""
-    return (open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
   name = 'dtit',
-  packages = ['dtit'], # this must be the same as the name above
+  packages = ['.'], # this must be the same as the name above
   version = '1.0',
   description = 'A decision-tree based conditional independence test',
   author = 'Krzysztof Chalupka',
   author_email = 'kjchalup@caltech.edu',
   license='MIT',
   install_requires=[
-      'numpy=',
-      'sklearn=0.0',
-      'scipy=0.19.0']
+      'numpy>=1.12.1',
+      'sklearn>=0.0',
+      'scipy>=0.19.0'],
   url = 'https://github.com/kjchalup/dtit',
+  download_url = 'https://github.com/kjchalup/dtit/archive/1.0tar.gz',
   keywords = ['graphical models', 'statistics',
       'machine learning', 'decision tree']
 )
